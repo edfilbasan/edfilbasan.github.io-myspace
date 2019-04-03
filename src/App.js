4d08701aa5ad047ddbd965b5fe8abd0d5f8eab21
Edfil Basan <edgdulay@gmail.com>
2019-04-03 14:35:12 -0700

    changing up the portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./styles/home.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        <div id="navigation">
+          <center>
+            <a
+              href="https://web.archive.org/web/20180723164813/http://takingbackemochicago.tumblr.com/"
+              className="links1"
+            >
+              HOME
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/myspaceemoprom"
+              className="links1"
+            >
+              MYSPACE.EMO.PROM
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/mcrtribute"
+              className="links1"
+            >
+              MCR.TRIBUTE
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/coverband"
+              className="links1"
+            >
+              COVERBAND
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/videodj"
+              className="links1"
+            >
+              DJ
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/TOUR"
+              className="links1"
+            >
+              TOUR
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://www.facebook.com/TakingBackEmo/"
+              className="links1"
+            >
+              [FB]
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://www.instagram.com/TakingBackEmo/"
+              className="links1"
+            >
+              [IG]
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://twitter.com/Taking_Back_Emo"
+              className="links1"
+            >
+              [TW]
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/TECH"
+              className="links1"
+            >
+              TECH
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/MAILTO:TAKINGBACKEMO@GMAIL.COM"
+              className="links1"
+            >
+              CONTACT
+            </a>
+          </center>
+        </div>
+        <table width="300" border="0" cellspacing="0" cellpadding="0"><td width="96" height="96" rowspan="2"><a href="https://web.archive.org/web/20180723164813/http://takingbackemochicago.tumblr.com/"><img src="https://web.archive.org/web/20180723164813im_/http://static.tumblr.com/d329d9c623f920e1566bcd92a238a091/f8mjs9r/HpUow84an/tumblr_static_7778r3lo9gcgggswk8844w0sg.jpg" align="left" width="200" style="float:left;padding:7px;"></a>    
+</table> </td>
+    </div>
     );
   }
 }
