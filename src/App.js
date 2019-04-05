0996a1cc3043482f37c1e133b89fdc1eb392af55
Edfil Basan <edgdulay@gmail.com>
2019-04-04 22:12:20 -0700

    cleaning up css

--- src/App.js
+++ src/App.js
@@ -19,63 +19,7 @@ class App extends Component {
               href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/myspaceemoprom"
               className="links1"
             >
-              MYSPACE.EMO.PROM
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/mcrtribute"
-              className="links1"
-            >
-              MCR.TRIBUTE
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/coverband"
-              className="links1"
-            >
-              COVERBAND
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/videodj"
-              className="links1"
-            >
-              DJ
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/TOUR"
-              className="links1"
-            >
-              TOUR
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://www.facebook.com/TakingBackEmo/"
-              className="links1"
-            >
-              [FB]
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://www.instagram.com/TakingBackEmo/"
-              className="links1"
-            >
-              [IG]
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://twitter.com/Taking_Back_Emo"
-              className="links1"
-            >
-              [TW]
-            </a>
-            |
-            <a
-              href="https://web.archive.org/web/20180723164813/https://takingbackemochicago.tumblr.com/TECH"
-              className="links1"
-            >
-              TECH
+              PROJECTS
             </a>
             |
             <a
@@ -84,11 +28,31 @@ class App extends Component {
             >
               CONTACT
             </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://www.facebook.com/TakingBackEmo/"
+              className="links1"
+            >
+              [TWITTER]
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://www.instagram.com/TakingBackEmo/"
+              className="links1"
+            >
+              [LINKEDIN]
+            </a>
+            |
+            <a
+              href="https://web.archive.org/web/20180723164813/https://twitter.com/Taking_Back_Emo"
+              className="links1"
+            >
+              [GITHUB]
+            </a>
+            |
           </center>
         </div>
-        <table width="300" border="0" cellspacing="0" cellpadding="0"><td width="96" height="96" rowspan="2"><a href="https://web.archive.org/web/20180723164813/http://takingbackemochicago.tumblr.com/"><img src="https://web.archive.org/web/20180723164813im_/http://static.tumblr.com/d329d9c623f920e1566bcd92a238a091/f8mjs9r/HpUow84an/tumblr_static_7778r3lo9gcgggswk8844w0sg.jpg" align="left" width="200" style="float:left;padding:7px;"></a>    
-</table> </td>
-    </div>
+      </div>
     );
   }
 }
