2912dcc52a362506094febf3f6766d8505852542
Edfil Basan <edgdulay@gmail.com>
2019-04-06 10:23:35 -0700

    profile component done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./page/home.js";
-import { BrowserRouter as Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 
 class App extends Component {
--- src/page/home.js
+++ src/page/home.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import Navigation from "../components/navigation.js";
+import Profile from "../components/profile.js";
+import "../styles/homeContent.css";
 
 class Home extends Component {
   render() {
-    return <Navigation />;
+    return (
+      <div>
+        <Navigation />
+        <div className="homeContent">
+          <Profile />
+          <h1 id="extendedNetwork">
+            Edfil Basan is a digital product designer. <br />He is also in your
+            extended network.
+          </h1>
+        </div>
+      </div>
+    );
   }
 }
 
