b42c3598ea8480d8ca9ba9d38b11092a0e35f02c
Edfil Basan <edgdulay@gmail.com>
2019-04-10 03:05:48 -0400

    Updates

---
+++ precache-manifest.3889abcf6db9fa648e78c607c23ffe94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96dfd43724381074833a",
+    "url": "/edfilbasan/static/js/runtime~main.c6c31d7b.js"
+  },
+  {
+    "revision": "ad90aab3ca64452ddecd",
+    "url": "/edfilbasan/static/js/main.78e9cd41.chunk.js"
+  },
+  {
+    "revision": "917fa33af82de413de12",
+    "url": "/edfilbasan/static/js/2.fa1d4533.chunk.js"
+  },
+  {
+    "revision": "ad90aab3ca64452ddecd",
+    "url": "/edfilbasan/static/css/main.4c379873.chunk.css"
+  },
+  {
+    "revision": "317e927cc3fc8a2192d133f2c772cd0e",
+    "url": "/edfilbasan/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/edfilbasan/edfilbasan/precache-manifest.c3bba58892ffeafb0bb2ed14ddd93e74.js"
+  "/edfilbasan/precache-manifest.3889abcf6db9fa648e78c607c23ffe94.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/edfilbasan/edfilbasan/index.html", {
+workbox.routing.registerNavigationRoute("/edfilbasan/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
