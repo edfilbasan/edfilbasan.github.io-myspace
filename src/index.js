2bd96fc4c4e0047edf5bb6f6caf7899bc3e61236
Edfil Basan <edgdulay@gmail.com>
2019-04-10 01:02:55 -0400

    typo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App.js";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL} r>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById("root")
