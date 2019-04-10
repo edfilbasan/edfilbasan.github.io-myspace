c54340c8e3d41017510cab46b594db9044a0af1c
Edfil Basan <edgdulay@gmail.com>
2019-04-10 18:58:55 -0400

    build folder changed to doc

---
+++ docs/precache-manifest.8932e7bdc9a868227e07d890522128f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8504008b0a6d71f548b",
+    "url": "/static/js/main.c89105e6.chunk.js"
+  },
+  {
+    "revision": "917fa33af82de413de12",
+    "url": "/static/js/2.fa1d4533.chunk.js"
+  },
+  {
+    "revision": "f8504008b0a6d71f548b",
+    "url": "/static/css/main.4c379873.chunk.css"
+  },
+  {
+    "revision": "5e20fd917b9239a941f219a4320a4e33",
+    "url": "/index.html"
+  }
+];
