36afc8e989d78eec25493ec26beaaffb8cb20d3b
Edfil Basan <edgdulay@gmail.com>
2019-04-11 01:48:01 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myspace/precache-manifest.97212181f84bd175d5775a9e657b6683.js"
+  "/work/precache-manifest.2a3339635eb5bf4d7ace53f1ae9481a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myspace/index.html", {
+workbox.routing.registerNavigationRoute("/work/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
