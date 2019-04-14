16ea1ad2f173788fd16a4284528a9a7832b73405
Edfil Basan <edgdulay@gmail.com>
2019-04-14 00:01:16 -0700

    Updates

---
+++ precache-manifest.b523ef8dba80cf7655aa5a67b74da456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2faa51981ada9eb26daf",
+    "url": "/work/static/js/runtime~main.84d4238b.js"
+  },
+  {
+    "revision": "64a63ecd318f02705545",
+    "url": "/work/static/js/main.55d143ea.chunk.js"
+  },
+  {
+    "revision": "e2f4dd80c9bd4dff3f8f",
+    "url": "/work/static/js/2.055e80db.chunk.js"
+  },
+  {
+    "revision": "64a63ecd318f02705545",
+    "url": "/work/static/css/main.c34f01db.chunk.css"
+  },
+  {
+    "revision": "502bb80d40747040a4835652e3e06b14",
+    "url": "/work/index.html"
+  }
+];
