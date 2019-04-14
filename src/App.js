336a2ab075cec3bab153b2063bf318c25b37905f
Edfil Basan <edgdulay@gmail.com>
2019-04-13 23:45:29 -0700

    okay wrapping it in Router doesn't do anything

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <Switch>
-            <Route exact path={"/"} component={Home} />
-            <Route exact path={"/construction"} component={Construction} />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/construction"} component={Construction} />
+        </Switch>
       </div>
     );
   }
